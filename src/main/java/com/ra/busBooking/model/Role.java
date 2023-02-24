6ce8dd97c92c813cfc7c99ba4d3df46c7f479ed4
Roz Maharjan <rox.maharjan@gmail.com>
2023-02-24 16:32:26 +0545

    complete project

---
+++ src/main/java/com/ra/busBooking/model/Role.java
@@ -0,0 +1,36 @@
+package com.ra.busBooking.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String role;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+	
+}
